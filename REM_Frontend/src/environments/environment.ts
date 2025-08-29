fe640bde203248f2c1e6f285940e9ec7dd065f87
RudrantUkirde <rudrantukirde97@gmail.com>
2025-08-29 19:54:17 +0530

    Deleted Docker file

--- REM_Frontend/src/environments/environment.ts
+++ REM_Frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production:false,
-  apiUrl: 'http://localhost:8080/' //Backend URL     
+  // apiUrl: 'http://localhost:8080/' //Backend URL
+  apiUrl: 'https://real-estate-management-production-31e7.up.railway.app/' //Backend URL
 };
